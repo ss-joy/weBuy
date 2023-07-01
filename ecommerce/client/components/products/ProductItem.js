731d0afe7ed668c544085e795ce1be91f4975b41
ss-joy <ssjoy.u.b@gmail.com>
2023-07-01 09:46:01 +0600

    fix: product item component

--- ecommerce/client/components/products/ProductItem.js
+++ ecommerce/client/components/products/ProductItem.js
@@ -10,18 +10,18 @@ function cutOutFirst100Words(text) {
 
 const ProductItem = ({ product }) => {
   return (
-    <li className="my-12 px-8 flex flex-row items-center justify-evenly">
+    <li className="mt-12 mb-20 px-8 flex flex-row items-center justify-evenly">
       <section className="w-1/3">
         <Image
           alt="Product image"
-          src={"/product-images/wallet.webp"}
+          src={product.imagePath}
           width={700}
           height={700}
         />
       </section>
       <section className="w-2/5 ">
-        <h2 className="text-6xl text-orange-600">{product.name}</h2>
-        <p className="">{cutOutFirst100Words(product.description)}</p>
+        <h2 className="text-6xl mb-20 text-orange-600">{product.name}</h2>
+        <p className="mb-12">{cutOutFirst100Words(product.description)}</p>
 
         <section className="flex items-center justify-between">
           <span className="p-3 rounded-md font-semibold text-white text-2xl bg-orange-400">
@@ -29,7 +29,7 @@ const ProductItem = ({ product }) => {
           </span>
           <Link
             className="bg-blue-500 text-white font-semibold p-5 rounded hover:bg-white hover:text-blue-800 hover:font-bold hover:shadow hover:shadow-blue-400"
-            href={"/product/idfix"}
+            href={`/products/${product._id}`}
           >
             View Product &#8594;
           </Link>
@@ -39,20 +39,4 @@ const ProductItem = ({ product }) => {
   );
 };
 
-{
-  /* <li className="m-4 mb-16 p-6 bg-orange-50 rounded-md shadow-2xl shadow-orange-300 flex flex-row justify-around items-center">
-<section>
-  <img
-    className="rounded-md"
-    src="https://img.freepik.com/free-photo/showing-cart-trolley-shopping-online-sign-graphic_53876-133967.jpg"
-    width={"27980px"}
-  />
-</section>
-<section>
-  <h2>{product.name}</h2>
-  <p className="h-40 overflow-hidden">{product.description}</p>
-  {product.price}
-</section>
-</li> */
-}
 export default ProductItem;
