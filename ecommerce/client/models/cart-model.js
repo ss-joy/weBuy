495eb678c1792a099bd1430a0c457e5d6fb7e8d4
ss-joy <ssjoy.u.b@gmail.com>
2023-07-01 06:39:09 +0600

    add cart model

---
+++ ecommerce/client/models/cart-model.js
@@ -0,0 +1,18 @@
+import { Schema, model, models } from "mongoose";
+
+const cartProductSchema = new Schema({
+  name: String,
+  id: String,
+  price: String,
+  quantity: String,
+});
+
+const cartSchema = new Schema({
+  userid: { type: String, required: true },
+
+  product: [cartProductSchema],
+});
+
+const Cart = models.Cart || model("Cart", cartSchema);
+
+export default Cart;
