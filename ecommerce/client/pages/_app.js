3f1032e0439b1ccfd5d18d4e403c0b38f91677ef
ss-joy <ssjoy.u.b@gmail.com>
2023-07-01 09:45:13 +0600

    add session provider

--- ecommerce/client/pages/_app.js
+++ ecommerce/client/pages/_app.js
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import MainLayout from "@/components/layouts/MainLayout";
 import MainHeader from "@/components/ui/MainHeader";
+import { Provider } from "next-auth/client";
+
 export default function App({ Component, pageProps }) {
   return (
-    <MainLayout>
-      <MainHeader />
-      <Component {...pageProps} />;
-    </MainLayout>
+    <Provider session={pageProps.session}>
+      <MainLayout>
+        <MainHeader />
+        <Component {...pageProps} />;
+      </MainLayout>
+    </Provider>
   );
 }
