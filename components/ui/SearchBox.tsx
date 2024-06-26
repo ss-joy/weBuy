062a351e99d4662c357a1f8d9c22f29123420fa9
asdfstudio <sss.joy.one@gmail.com>
2024-06-26 19:18:11 +0600

    fix: fix search functionality

--- components/ui/SearchBox.tsx
+++ components/ui/SearchBox.tsx
@@ -1,79 +1,51 @@
 //version 1.0.1
 import { SearchIcon } from "lucide-react";
-import React, { useEffect, useState, useRef } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { cn } from "@/lib";
-
-interface Product {
-  _id: string;
-  name: string;
-  description: string;
-  price: number;
-  imagePath: string;
-  sellerName: string;
-  sellerId: string;
-  sellCount: number;
-}
+import { useQuery } from "@tanstack/react-query";
+import { makeGetRequest } from "@/queries";
+import { ApiResponse } from "@/types/apiResponse";
+import { productSchemaType } from "@/types/products-type";
 
 function SearchBox() {
+  const [input, setInput] = useState<string>("");
   const [searchValue, setSearchValue] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [data, setData] = useState<Product[]>([]);
-  const [selectedItemIndex, setSelectedItemIndex] = useState<number | null>(
-    null
-  );
-  const searchResultsRef = useRef<HTMLUListElement>(null);
+  const [showSearchResults, setShowSearchResults] = useState<boolean>(false);
 
   useEffect(() => {
-    async function getProducts() {
-      try {
-        setIsLoading(true);
-        const qdata = await axios.get(
-          `/api/products/search?productName=${searchValue}`
-        );
-        setData(qdata?.data.data);
-        setIsLoading(false);
-      } catch (error) {
-        setIsLoading(false);
-        setData([]);
-        console.error("Error fetching data:", error);
-      }
+    let waitInputValue = "";
+    if (input) {
+      waitInputValue = input;
     }
-
-    const clearPrevCall = setTimeout(() => {
-      getProducts();
-    }, 500);
+    const v = setTimeout(() => {
+      setSearchValue(waitInputValue);
+    }, 2000);
 
     return () => {
-      clearTimeout(clearPrevCall);
+      clearTimeout(v);
     };
-  }, [searchValue]);
+  }, [input]);
 
-  function handleSearchInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-    setSearchValue(e.target.value);
-    setSelectedItemIndex(null);
+  interface SearchResult extends ApiResponse {
+    data: productSchemaType[];
   }
 
-  function handleKeyDown(e: React.KeyboardEvent) {
-    if (data && data.length > 0 && searchResultsRef.current) {
-      const numResults = data.length;
+  const { data, isFetching } = useQuery<SearchResult>({
+    queryKey: ["search-for-product", searchValue],
+    queryFn: () =>
+      makeGetRequest(`/api/products/search?productName=${searchValue}`),
+    enabled: !!(searchValue !== ""),
+  });
 
-      if (e.key === "ArrowDown") {
-        e.preventDefault();
-        setSelectedItemIndex((prevIndex) =>
-          prevIndex === null ? 0 : (prevIndex + 1) % numResults
-        );
-      } else if (e.key === "ArrowUp") {
-        e.preventDefault();
-        setSelectedItemIndex((prevIndex) =>
-          prevIndex === null
-            ? numResults - 1
-            : (prevIndex - 1 + numResults) % numResults
-        );
-      } else if (e.key === "Enter" && selectedItemIndex !== null) {
-      }
+  useEffect(() => {
+    if (data?.data) {
+      setShowSearchResults(true);
     }
+  }, [data]);
+
+  function handleSearchInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setInput(e.target.value);
   }
 
   return (
@@ -85,158 +57,40 @@ function SearchBox() {
         <span className="flex items-center lg:text-3xl w-full">
           <SearchIcon
             className={cn("text-slate-500 mr-2 h-[40px]", {
-              "animate-spin": isLoading,
+              "animate-spin": isFetching,
             })}
           />
           <input
             className="w-full h-[40px] focus:outline-2 outline-slate-500 pl-3 pr-2"
             onChange={handleSearchInputChange}
-            onKeyDown={handleKeyDown}
-            value={searchValue}
+            value={input}
             type="search"
             placeholder="search..."
-            aria-autocomplete="list"
-            aria-expanded={data.length > 0}
-            aria-owns="search-results"
           />
         </span>
-
-        <ul
-          id="search-results"
-          ref={searchResultsRef}
-          className={cn(
-            "absolute z-50 top-[60px] left-[-60px] shadow-lg shadow-slate-400 rounded-md bg-white border-2 border-blue-300 w-[300px] md:w-full",
-            {
-              hidden: data.length === 0,
-            }
-          )}
-          role="listbox"
-        >
-          {data?.map((e, index) => (
-            <div key={e._id} className="w-full">
-              <li
-                className={`shadow my-1 p-1 text-center w-full transition-all bg-white hover:bg-slate-300 text-slate-700 rounded hover:text-white hover:font-bold text-xl overflow-clip ${
-                  index === selectedItemIndex ? "bg-gray-200" : ""
-                }`}
-                role="option"
-                tabIndex={0}
-              >
-                <Link
-                  className=""
-                  onClick={() => {
-                    setData([]);
-                  }}
-                  href={`/products/${e._id}`}
+        {data && showSearchResults ? (
+          <ul
+            className={cn(
+              "absolute z-50 top-[60px] left-0 shadow-lg shadow-slate-400 rounded-md bg-white border-2 border-blue-300 w-full"
+            )}
+          >
+            {data?.data.map((e, index) => (
+              <div key={e._id} className="w-full">
+                <li
+                  className={`shadow my-1 p-1 text-center w-full transition-all bg-white hover:bg-slate-300 text-slate-700 rounded hover:text-white hover:font-bold text-xl overflow-clip`}
+                  onClick={() => setShowSearchResults(false)}
                 >
-                  {e.name}
-                </Link>
-              </li>
-            </div>
-          ))}
-        </ul>
+                  <Link className="" href={`/products/${e._id}`}>
+                    {e.name}
+                  </Link>
+                </li>
+              </div>
+            ))}
+          </ul>
+        ) : null}
       </div>
     </>
   );
 }
 
 export default SearchBox;
-
-// version 1.0.0
-// import { SearchIcon } from "lucide-react";
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import Link from "next/link";
-// import { cn } from "@/lib";
-// interface Product {
-//   _id: string;
-//   name: string;
-//   description: string;
-//   price: number;
-//   imagePath: string;
-//   sellerName: string;
-//   sellerId: string;
-//   sellCount: number;
-// }
-
-// function SearchBox() {
-//   const [searchValue, setSearchValue] = useState<string>("");
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-//   const [data, setData] = useState<Product[]>([]);
-
-//   useEffect(() => {
-//     async function getProducts() {
-//       try {
-//         setIsLoading(true);
-//         const qdata = await axios.get(
-//           `http://localhost:3000/api/products/search?productName=${searchValue}`
-//         );
-//         setData(qdata?.data.data);
-//         setIsLoading(false);
-//       } catch (error) {
-//         setIsLoading(false);
-//         setData([]);
-//         console.log(error);
-//       }
-//     }
-
-//     const clearPrevCall = setTimeout(() => {
-//       getProducts();
-//     }, 500);
-
-//     return () => {
-//       clearTimeout(clearPrevCall);
-//     };
-//   }, [searchValue]);
-
-//   function handleSearchInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-//     setSearchValue(e.target.value);
-//   }
-//   return (
-//     <>
-//       <div
-//         id="searchbox-container"
-//         className="relative w-[10px] md:w-[350px] lg:w-[450px]"
-//       >
-//         <span className="flex items-center lg:text-3xl w-full">
-//           <SearchIcon
-//             className={cn("text-slate-500 mr-2 h-[40px]", {
-//               "animate-spin": isLoading,
-//             })}
-//           />
-//           <input
-//             className="w-full h-[40px] focus:outline-2 outline-slate-500 pl-3"
-//             onChange={handleSearchInputChange}
-//             value={searchValue}
-//             type="search"
-//             placeholder="search..."
-//           />
-//         </span>
-
-//         <ul
-//           className={cn(
-//             "absolute top-[60px] left-[-60px] shadow-lg shadow-slate-400 rounded-md bg-white border-2 border-blue-300 w-[300px] md:w-full",
-//             {
-//               hidden: data.length === 0,
-//             }
-//           )}
-//         >
-//           {data?.map((e) => {
-//             return (
-//               <div key={e._id} className="w-full">
-//                 <li className="shadow my-1 p-1 text-center w-full transition-all bg-white hover:bg-slate-300 text-slate-700 rounded hover:text-white hover:font-bold text-xl overflow-clip">
-//                   {
-//                     <Link className="" href={`/products/search/${e._id}`}>
-//                       {e.name}
-//                     </Link>
-//                   }
-//                 </li>
-//               </div>
-//             );
-//           })}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default SearchBox;
